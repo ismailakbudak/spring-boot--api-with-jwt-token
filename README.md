f6841fb25a445c61548b4d40723b4d28c900952d
İsmail Akbudak <akbudak.ismail@yandex.com>
2018-12-05 20:32:11 +0300

    Implement client end point feature

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
-## Login As Merchant
+# Spring Boot API Example App with JWT Token Authorization 
+
+## API End Points
+### Login As Merchant
 ```
 curl -i -H "Content-Type: application/json" -X POST -d '{
     "email": "merchant@dev.com",
     "password": "password" 
 }' https://reporting-service-api.herokuapp.com/api/v3/merchant/user/login
 ```
-Set Authorization token in console for further usage
+Set Authorization token in  your terminal to use in below curl requests
 ```
 $ YOUR_TOKEN=set_your_token
 $ echo $YOUR_TOKEN
 ```
 
-## Transactions Report
+#### Transactions Report
 - Implemented all feature of this end point
 ```
 curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X POST -d '{
@@ -19,7 +22,7 @@ curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X P
     "endDate": "2060-01-04"
 }' https://reporting-service-api.herokuapp.com/api/v3/transactions/report
 ```
-Use all filter queries
+You can use all filters
 ```
 curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X POST -d '{
     "fromDate": "2018-12-04", 
@@ -29,8 +32,33 @@ curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X P
 }' https://reporting-service-api.herokuapp.com/api/v3/transactions/report
 ```
 
+#### Transaction Detail
+- Implemented all feature of this end point
+```
+curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X POST -d '{
+    "transactionId": "transaction_10"
+}' https://reporting-service-api.herokuapp.com/api/v3/transaction
+```
+```
+curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X POST -d '{
+    "transactionId": "not_exists_transaction"
+}' https://reporting-service-api.herokuapp.com/api/v3/transaction
+```
 
-## Transaction List
+#### Client Detail
+- Implemented all feature of this end point
+```
+curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X POST -d '{
+    "transactionId": "transaction_10"
+}' https://reporting-service-api.herokuapp.com/api/v3/client
+```
+```
+curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X POST -d '{
+    "transactionId": "not_exists_transaction"
+}' https://reporting-service-api.herokuapp.com/api/v3/client
+```
+
+#### Transaction List
 - Not implemented all feature of this end point
 ```
 curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X POST -d '{
@@ -38,3 +66,21 @@ curl -i -H "Content-Type: application/json" -H "Authorization: $YOUR_TOKEN" -X P
     "endDate": "2060-01-04"
 }' https://reporting-service-api.herokuapp.com/api/v3/transaction/list
 ```
+
+## Development
+- Assembles and tests this project
+```
+$ gradle build
+```
+- Runs the unit tests
+```
+$ gradle test
+```
+- Runs this project as a Spring Boot application
+```
+$ gradle bootRun
+```
+
+## License
+
+Copyright (c) 2018 İsmail Akbudak
---
+++ src/main/java/report/service/v3/response/ClientResponse.java
@@ -0,0 +1,65 @@
+package report.service.v3.response;
+
+import report.service.v3.dto.*;
+import report.service.v3.exception.TransactionNotFoundException;
+import report.service.v3.model.CustomerInfo;
+import report.service.v3.model.Transaction;
+import report.service.v3.request.TransactionRequest;
+import report.service.v3.service.TransactionService;
+
+public class ClientResponse {
+    private CustomerInfoDTO customerInfo;
+
+    public ClientResponse(TransactionService transactionService, TransactionRequest transactionRequest) throws TransactionNotFoundException {
+        Transaction transactionModel = transactionService.findTransactionByTransactionId(transactionRequest.getTransactionId());
+
+        // Customer Info
+        CustomerInfo transactionCustomer = transactionModel.getCustomerInfo();
+        customerInfo = new CustomerInfoDTO();
+        customerInfo.setId(transactionCustomer.getId());
+        customerInfo.setNumber(transactionCustomer.getNumber());
+        customerInfo.setExpiryMonth(transactionCustomer.getExpiryMonth());
+        customerInfo.setExpiryYear(transactionCustomer.getExpiryYear());
+        customerInfo.setStartMonth(transactionCustomer.getStartMonth());
+        customerInfo.setStartYear(transactionCustomer.getStartYear());
+        customerInfo.setIssueNumber(transactionCustomer.getIssueNumber());
+        customerInfo.setEmail(transactionCustomer.getEmail());
+        customerInfo.setBirthday(transactionCustomer.getBirthday());
+        customerInfo.setGender(transactionCustomer.getGender());
+        customerInfo.setBillingTitle(transactionCustomer.getBillingTitle());
+        customerInfo.setBillingFirstName(transactionCustomer.getBillingFirstName());
+        customerInfo.setBillingLastName(transactionCustomer.getBillingLastName());
+        customerInfo.setBillingCompany(transactionCustomer.getBillingCompany());
+        customerInfo.setBillingAddress1(transactionCustomer.getBillingAddress1());
+        customerInfo.setBillingAddress2(transactionCustomer.getBillingAddress2());
+        customerInfo.setBillingCity(transactionCustomer.getBillingCity());
+        customerInfo.setBillingPostcode(transactionCustomer.getBillingPostcode());
+        customerInfo.setBillingState(transactionCustomer.getBillingState());
+        customerInfo.setBillingCountry(transactionCustomer.getBillingCountry());
+        customerInfo.setBillingPhone​(transactionCustomer.getBillingPhone​());
+        customerInfo.setBillingFax(transactionCustomer.getBillingFax());
+        customerInfo.setShippingTitle(transactionCustomer.getShippingTitle());
+        customerInfo.setShippingFirstName(transactionCustomer.getShippingFirstName());
+        customerInfo.setShippingLastName​(transactionCustomer.getShippingLastName​());
+        customerInfo.setShippingCompany(transactionCustomer.getShippingCompany());
+        customerInfo.setShippingAddress1​(transactionCustomer.getShippingAddress1​());
+        customerInfo.setShippingAddress2​(transactionCustomer.getShippingAddress2​());
+        customerInfo.setShippingCity​(transactionCustomer.getShippingCity​());
+        customerInfo.setShippingPostcode​(transactionCustomer.getShippingPostcode​());
+        customerInfo.setShippingState(transactionCustomer.getShippingState());
+        customerInfo.setShippingCountry(transactionCustomer.getShippingCountry());
+        customerInfo.setShippingPhone(transactionCustomer.getShippingPhone());
+        customerInfo.setShippingFax(transactionCustomer.getShippingFax());
+        customerInfo.setToken(transactionCustomer.getToken());
+        customerInfo.setCreatedAt(transactionCustomer.getCreatedAt());
+        customerInfo.setUpdatedAt(transactionCustomer.getUpdatedAt());
+    }
+
+    public CustomerInfoDTO getCustomerInfo() {
+        return customerInfo;
+    }
+
+    public void setCustomerInfo(CustomerInfoDTO customerInfo) {
+        this.customerInfo = customerInfo;
+    }
+}
